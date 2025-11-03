da483424ce72da8765c5c5aa5d7caf22551684c6
Aniket-Bhilare7227 <aniketbhilare17@gmail.com>
2025-11-03 20:01:30 +0530

    Update Footer.jsx

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -127,7 +127,7 @@ const Footer = () => {
               © 2025 SilverlineTC. Thank you Aniket.
             </p>
             <p className="text-primary-foreground/60">
-           Testing Server.
+           Testing Pipeline.
             </p>
           </div>
         </div>
