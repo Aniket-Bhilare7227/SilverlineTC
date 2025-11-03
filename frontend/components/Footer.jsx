3a5998a6d859318f23edd6cd6da4590a82ec752d
Aniket-Bhilare7227 <aniketbhilare17@gmail.com>
2025-11-03 19:22:46 +0530

    Update footer text from 'Testing Demo' to 'Testing purpose'

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -127,7 +127,7 @@ const Footer = () => {
               © 2025 SilverlineTC. Thanks Aniket.
             </p>
             <p className="text-primary-foreground/60">
-           Testing Demo
+           Testing purpose
             </p>
           </div>
         </div>
